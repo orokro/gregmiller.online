fc4afa358d351c10509a0d51bc9b2f78f574f938
Greg Miller <hypurban86@gmail.com>
2026-01-09 23:34:08 -0500

    corrects pages location

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
-})
+	compatibilityDate: '2025-07-15',
+	devtools: { enabled: true },
+
+	// THIS IS CRITICAL for the app/ folder structure
+	future: {
+		compatibilityVersion: 4,
+	},
+});
--- pages/index.vue
+++
@@ -1,6 +0,0 @@
-<template>
-	<div>
-		<h1>Local Dev Environment</h1>
-		<p>This is coming from my local machine!</p>
-	</div>
-</template>
