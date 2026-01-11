c7b8aef001e2545c7c0044bfd62fee66b2d904f5
Greg Miller <hypurban86@gmail.com>
2026-01-10 22:17:30 -0500

    Adds 3d test page, more formatting

--- app/app.vue
+++ app/app.vue
@@ -1,3 +1,9 @@
+<!--
+	app.vue
+	------
+
+	The root component for the application. This is where we can set up global styles, headers, footers, etc.
+-->
 <template>
 	<div>
         <AppHeader />
---
+++ app/pages/3d_test.vue
@@ -0,0 +1,26 @@
+<!--
+	3d_test.vue
+	-----------
+
+	A test page for 3D content. This is a placeholder while I experiment with 3D libraries and content.
+-->
+<script setup>
+
+
+</script>
+<template>
+
+	<div class="static-page">
+		<h1>{{ $route.path }}</h1>
+		<p>🚧 3D test page. Content coming soon.</p>
+		<p v-if="$route.hash">Anchor requested: {{ $route.hash }}</p>
+	</div>
+</template>
+<style scoped>
+
+.static-page {
+	padding: 4rem;
+	text-align: center;
+	font-family: sans-serif;
+}
+</style>
