e17a6805b31b23f765fbd9156248ba8c363ae03c
Greg Miller <hypurban86@gmail.com>
2026-01-15 08:17:05 -0500

    Finishes all main pages in Sidebar except home

---
+++ app/pages/[category]/technology-reviews.vue
@@ -0,0 +1,45 @@
+<!--
+	technology-reviews.vue
+	----------------------
+
+	The Technology Reviews page.
+-->
+<script setup>
+
+// vue
+import { ref } from 'vue';
+
+// components
+import Container3D from '../../components/Container3D.vue';
+import Spacer3D from '../../components/Spacer3D.vue';
+import CategoryGrid from '../../app/components/CategoryGrid.vue';
+
+</script>
+<template>
+
+	<div class="static-page">
+
+		<Container3D>
+			<h1><span>Technology Reviews</span></h1>
+			<div class="white-box text">
+
+				While I don't update this too frequently, I have written many reviews of technology products over the years, including software, hardware, and gadgets. I enjoy sharing my thoughts and experiences with technology, and hope that my reviews can help others make informed decisions about the products they use.
+				<br/><br/>
+				Perhaps you'll find something useful, or appreciate a snapshot of what was once cutting edge technology. Either way, I hope you enjoy reading my reviews as much as I enjoyed writing them.
+			</div>
+		</Container3D>
+
+		<Spacer3D/>
+
+		<CategoryGrid title="Technology Reviews" category="Technology Reviews" />
+	</div>
+</template>
+<style lang="scss" scoped>
+
+.white-box {
+
+	padding: 2rem 1rem;;
+
+}// .white-box
+
+</style>
---
+++ app/pages/[category]/uncategorized.vue
@@ -0,0 +1,47 @@
+<!--
+	uncategorized.vue
+	-----------------
+
+	The Uncategorized page.
+-->
+<script setup>
+
+// vue
+import { ref } from 'vue';
+
+// components
+import Container3D from '../../components/Container3D.vue';
+import Spacer3D from '../../components/Spacer3D.vue';
+import CategoryGrid from '../../app/components/CategoryGrid.vue';
+
+</script>
+<template>
+
+	<div class="static-page">
+
+		<Container3D>
+			<h1><span>Other</span></h1>
+			<div class="white-box text">
+				Here's some uncategorized stuff that doesn't fit in another category.
+			</div>
+		</Container3D>
+
+		<Spacer3D/>
+
+		<CategoryGrid title="Uncategorized" category="Uncategorized" />
+
+		<Spacer3D/>
+
+		<CategoryGrid title="Geocaching" category="Geocaching" />
+
+	</div>
+</template>
+<style lang="scss" scoped>
+
+.white-box {
+
+	padding: 2rem 1rem;;
+
+}// .white-box
+
+</style>
